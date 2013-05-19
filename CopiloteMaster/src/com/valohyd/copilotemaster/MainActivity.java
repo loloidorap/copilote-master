ca7e761aa7bc7e4a0c0eb30a5ad1ff5868b451b1
parodilaurent <parodilaurent@gmail.com>
2013-05-19 19:13:51 +0000

    Statut GPS

--- CopiloteMaster/src/com/valohyd/copilotemaster/MainActivity.java
+++ CopiloteMaster/src/com/valohyd/copilotemaster/MainActivity.java
@@ -1,9 +1,5 @@
 package com.valohyd.copilotemaster;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.widget.Toast;
