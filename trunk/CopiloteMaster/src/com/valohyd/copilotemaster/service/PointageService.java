2d2730280f44141b4a55fbb1bdc0f36b66056dd9
parodilaurent@gmail.com <parodilaurent@gmail.com@57eff9ca-b42b-2320-a783-d92fe28ad753>
2013-05-20 14:37:21 +0000

    Update des notifs OK

--- trunk/CopiloteMaster/src/com/valohyd/copilotemaster/service/PointageService.java
+++ trunk/CopiloteMaster/src/com/valohyd/copilotemaster/service/PointageService.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.os.Binder;
 import android.os.CountDownTimer;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.valohyd.copilotemaster.MainActivity;
@@ -23,165 +24,164 @@ import com.valohyd.copilotemaster.fragments.PointageFragment;
  * 
  */
 public class PointageService extends Service {
-    private final IBinder mBinder = new MyBinder(); // binder pour accéder au
-                                                    // service
-    private CountDownTimer remainTimer; // Temps restants dans le temps imparti
-    private PointageFragment hook; // hook pour intéragir avec le fragment
+	private final IBinder mBinder = new MyBinder(); // binder pour accéder au
+													// service
+	private CountDownTimer remainTimer; // Temps restants dans le temps imparti
+	private PointageFragment hook; // hook pour intéragir avec le fragment
 
-    // notification manager
-    private NotificationManager notificationManager;
-    private Intent intent; // intent lors
-                           // du click
-                           // sur notif
-    private PendingIntent pIntent;
+	// notification manager
+	private NotificationManager notificationManager;
+	private NotificationCompat.Builder notif;
+	private Intent intent; // intent lors
+							// du click
+							// sur notif
+	private PendingIntent pIntent;
 
-    private long seconds;
-    private Timer timer; // timer for the past time
+	private long seconds;
+	private Timer timer; // timer for the past time
 
-    @Override
-    public IBinder onBind(Intent arg0) {
-        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        intent = new Intent(this, MainActivity.class);
-        pIntent = PendingIntent.getActivity(this, 0, intent, 0);
-        return mBinder;
-    }
+	@Override
+	public IBinder onBind(Intent arg0) {
+		notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+		intent = new Intent(this, MainActivity.class);
+		pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+		notif = new NotificationCompat.Builder(PointageService.this);
+		notif.setContentTitle("Pointage").setSmallIcon(R.drawable.ic_launcher)
+				.setContentIntent(pIntent).build();
+		return mBinder;
+	}
 
-    public CountDownTimer getRemainTimer() {
-        return remainTimer;
-    }
+	public CountDownTimer getRemainTimer() {
+		return remainTimer;
+	}
 
-    public void setHook(PointageFragment hook) {
-        this.hook = hook;
-    }
+	public void setHook(PointageFragment hook) {
+		this.hook = hook;
+	}
 
-    /**
-     * start le remainTimer et appelle le onTick et onFinish de PointageFragment
-     * 
-     * @param millisInFuture
-     * @param countDownInterval
-     */
-    public void startCountDownTimer(final long millisInFuture, long countDownInterval) {
-        // stop the other timer
-        stopCountDownTimer();
-        stopPastTimer();
-        
-        //start the new
-        remainTimer = new CountDownTimer(millisInFuture, countDownInterval) {
+	/**
+	 * start le remainTimer et appelle le onTick et onFinish de PointageFragment
+	 * 
+	 * @param millisInFuture
+	 * @param countDownInterval
+	 */
+	public void startCountDownTimer(final long millisInFuture,
+			long countDownInterval) {
+		// stop the other timer
+		stopCountDownTimer();
+		stopPastTimer();
 
-            @Override
-            public void onTick(long millisUntilFinished) {
-                if (hook != null) {
-                    hook.onTick(millisUntilFinished);
-                }
-                // Decoupage du temps restant pour l'affichage
-                long sec = (millisUntilFinished / 1000) % 60;
-                String secondes = "" + sec;
-                if (sec < 10) {
-                    secondes = "0" + sec;
-                }
-                long min = (millisUntilFinished / (1000 * 60)) % 60;
-                String minutes = "" + min;
-                if (min < 10) {
-                    minutes = "0" + min;
-                }
-                long hrs = (millisUntilFinished / (1000 * 60 * 60)) % 24;
-                String hours = "" + hrs;
-                if (hrs < 10) {
-                    hours = "0" + hrs;
-                }
-                Notification noti = new Notification.Builder(PointageService.this).setContentTitle("Pointage")
-                        .setContentText("temps : " + hours + ":" + minutes + ":" + secondes)
-                        .setSmallIcon(R.drawable.gps_started).setContentIntent(pIntent).build();
-                // .addAction(R.drawable.icon, "Call", pIntent)
-                // .addAction(R.drawable.icon, "More", pIntent)
-                // .addAction(R.drawable.icon, "And more", pIntent).build();
-                notificationManager.notify(0, noti);
-            }
+		// start the new
+		remainTimer = new CountDownTimer(millisInFuture, countDownInterval) {
 
-            @Override
-            public void onFinish() {
-                if (hook != null) {
-                    hook.onFinish(millisInFuture);
-                }
-                // start the past timer
-                startPastTimer(millisInFuture);
-            }
-        }.start();
-    }
+			@Override
+			public void onTick(long millisUntilFinished) {
+				if (hook != null) {
+					hook.onTick(millisUntilFinished);
+				}
+				// Decoupage du temps restant pour l'affichage
+				long sec = (millisUntilFinished / 1000) % 60;
+				String secondes = "" + sec;
+				if (sec < 10) {
+					secondes = "0" + sec;
+				}
+				long min = (millisUntilFinished / (1000 * 60)) % 60;
+				String minutes = "" + min;
+				if (min < 10) {
+					minutes = "0" + min;
+				}
+				long hrs = (millisUntilFinished / (1000 * 60 * 60)) % 24;
+				String hours = "" + hrs;
+				if (hrs < 10) {
+					hours = "0" + hrs;
+				}
+				notif.setContentText("temps : -" + hours + ":" + minutes + ":"
+						+ secondes);
+				notificationManager.notify(0, notif.build());
+			}
 
-    /**
-     * stop le count down timer
-     */
-    public void stopCountDownTimer() {
-        // Si le timer est deja actif
-        if (remainTimer != null)
-            remainTimer.cancel(); // On le stoppe
-    }
+			@Override
+			public void onFinish() {
+				if (hook != null) {
+					hook.onFinish(millisInFuture);
+				}
+				// start the past timer
+				startPastTimer(millisInFuture);
+			}
+		}.start();
+	}
 
-    public void startPastTimer(long millisSecondes){
-        // démarrer le timer au temps passé à 0
-        if(millisSecondes < 0){
-            seconds = -millisSecondes / 1000;
-        }else seconds = 0;
-        
-        // arrete le timer si il y en a dejà un de lancé
-        if(timer != null){
-            timer.cancel();
-            timer.purge();
-        }
-        // lancer un nouveau timer
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
+	/**
+	 * stop le count down timer
+	 */
+	public void stopCountDownTimer() {
+		// Si le timer est deja actif
+		if (remainTimer != null)
+			remainTimer.cancel(); // On le stoppe
+	}
 
-            @Override
-            public void run() {
-                Log.d("TIMER", "run");
-                seconds++;
+	public void startPastTimer(long millisSecondes) {
+		// démarrer le timer au temps passé à 0
+		if (millisSecondes < 0) {
+			seconds = -millisSecondes / 1000;
+		} else
+			seconds = 0;
 
-                // Decoupage du temps écoulé pour l'affichage
-                long sec = seconds % 60;
-                String secondes = "" + sec;
-                if (sec < 10) {
-                    secondes = "0" + sec;
-                }
-                long min = (seconds / 60) % 60;
-                String minutes = "" + min;
-                if (min < 10) {
-                    minutes = "0" + min;
-                }
-                long hrs = (seconds / 3600) % 24;
-                String hours = "" + hrs;
-                if (hrs < 10) {
-                    hours = "0" + hrs;
-                }
-                Notification noti = new Notification.Builder(PointageService.this).setContentTitle("Pointage")
-                        .setContentText("temps : " + hours + ":" + minutes + ":" + secondes)
-                        .setSmallIcon(R.drawable.gps_started).setContentIntent(pIntent).build();
-                // .addAction(R.drawable.icon, "Call", pIntent)
-                // .addAction(R.drawable.icon, "More", pIntent)
-                // .addAction(R.drawable.icon, "And more", pIntent).build();
-                notificationManager.notify(0, noti);
-            }
-        }, 1000, 1000);
-    }
+		// arrete le timer si il y en a dejà un de lancé
+		if (timer != null) {
+			timer.cancel();
+			timer.purge();
+		}
+		// lancer un nouveau timer
+		timer = new Timer();
+		timer.schedule(new TimerTask() {
 
-    public void stopPastTimer() {
-        // arrete le timer si il y en a dejà un de lancé
-        if (timer != null) {
-            timer.cancel();
-            timer.purge();
-        }
-    }
+			@Override
+			public void run() {
+				Log.d("TIMER", "run");
+				seconds++;
 
-    /**
-     * classe du binder, pour obtenir le service
-     * 
-     * @author Valentin Lecavelier
-     * 
-     */
-    public class MyBinder extends Binder {
-        public PointageService getService() {
-            return PointageService.this;
-        }
-    }
+				// Decoupage du temps écoulé pour l'affichage
+				long sec = seconds % 60;
+				String secondes = "" + sec;
+				if (sec < 10) {
+					secondes = "0" + sec;
+				}
+				long min = (seconds / 60) % 60;
+				String minutes = "" + min;
+				if (min < 10) {
+					minutes = "0" + min;
+				}
+				long hrs = (seconds / 3600) % 24;
+				String hours = "" + hrs;
+				if (hrs < 10) {
+					hours = "0" + hrs;
+				}
+
+				notif.setContentText("ATTENTION ! Temps : +" + hours + ":" + minutes + ":"
+						+ secondes);
+				notificationManager.notify(0, notif.build());
+			}
+		}, 1000, 1000);
+	}
+
+	public void stopPastTimer() {
+		// arrete le timer si il y en a dejà un de lancé
+		if (timer != null) {
+			timer.cancel();
+			timer.purge();
+		}
+	}
+
+	/**
+	 * classe du binder, pour obtenir le service
+	 * 
+	 * @author Valentin Lecavelier
+	 * 
+	 */
+	public class MyBinder extends Binder {
+		public PointageService getService() {
+			return PointageService.this;
+		}
+	}
 }
