42d9c259c8d792d0acc01d735f328da826872cbd
parodilaurent@gmail.com <parodilaurent@gmail.com@57eff9ca-b42b-2320-a783-d92fe28ad753>
2013-05-24 08:41:52 +0000

    MAJ Chrono avec millis

--- trunk/CopiloteMaster/src/com/valohyd/copilotemaster/utils/Chronometer.java
+++ trunk/CopiloteMaster/src/com/valohyd/copilotemaster/utils/Chronometer.java
@@ -4,17 +4,15 @@ package com.valohyd.copilotemaster.utils;
  * The Android chronometer widget revised so as to count milliseconds
  */
 
+import java.text.DecimalFormat;
 import java.util.Formatter;
-import java.util.IllegalFormatException;
 import java.util.Locale;
 
 import android.content.Context;
 import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
-import android.text.format.DateUtils;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.TextView;
 
 public class Chronometer extends TextView {
@@ -111,7 +109,7 @@ public class Chronometer extends TextView {
 	public void setFormat(String format) {
 		mFormat = format;
 		if (format != null && mFormatBuilder == null) {
-			mFormatBuilder = new StringBuilder(format.length() * 2);
+			mFormatBuilder = new StringBuilder(format.length() * 3);
 		}
 	}
 
@@ -191,28 +189,30 @@ public class Chronometer extends TextView {
 	}
 
 	private synchronized void updateText(long now) {
-		long seconds = now - mBase;
-		seconds /= 1000;
-		String text = DateUtils.formatElapsedTime(mRecycle, seconds);
+		long timeElapsed = now - mBase;
 
-		if (mFormat != null) {
-			Locale loc = Locale.getDefault();
-			if (mFormatter == null || !loc.equals(mFormatterLocale)) {
-				mFormatterLocale = loc;
-				mFormatter = new Formatter(mFormatBuilder, loc);
-			}
-			mFormatBuilder.setLength(0);
-			mFormatterArgs[0] = text;
-			try {
-				mFormatter.format(mFormat, mFormatterArgs);
-				text = mFormatBuilder.toString();
-			} catch (IllegalFormatException ex) {
-				if (!mLogged) {
-					Log.w(TAG, "Illegal format string: " + mFormat);
-					mLogged = true;
-				}
-			}
+		DecimalFormat df = new DecimalFormat("00");
+
+		int hours = (int) (timeElapsed / (3600 * 1000));
+		int remaining = (int) (timeElapsed % (3600 * 1000));
+
+		int minutes = (int) (remaining / (60 * 1000));
+		remaining = (int) (remaining % (60 * 1000));
+
+		int seconds = (int) (remaining / 1000);
+		remaining = (int) (remaining % (1000));
+
+		int milliseconds = (int) (((int) timeElapsed % 1000) / 100);
+
+		String text = "";
+
+		if (hours > 0) {
+			text += df.format(hours) + ":";
 		}
+
+		text += df.format(minutes) + ":";
+		text += df.format(seconds) + ":";
+		text += Integer.toString(milliseconds);
 		setText(text);
 	}
 
@@ -223,7 +223,7 @@ public class Chronometer extends TextView {
 				updateText(SystemClock.elapsedRealtime());
 				dispatchChronometerTick();
 				mHandler.sendMessageDelayed(
-						Message.obtain(mHandler, TICK_WHAT), 1000);
+						Message.obtain(mHandler, TICK_WHAT), 100);
 			} else {
 				mHandler.removeMessages(TICK_WHAT);
 			}
@@ -236,7 +236,7 @@ public class Chronometer extends TextView {
 			if (mRunning) {
 				updateText(SystemClock.elapsedRealtime());
 				dispatchChronometerTick();
-				sendMessageDelayed(Message.obtain(this, TICK_WHAT), 1000);
+				sendMessageDelayed(Message.obtain(this, TICK_WHAT), 100);
 			}
 		}
 	};
